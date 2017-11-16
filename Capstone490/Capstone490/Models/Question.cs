0dfb4a26dc861e5e777b121d666aa74cc912d27b
Kal <k.radikov@queensu.ca>
2017-11-15 20:05:27 -0500

    first upload
    Initial upload of code

---
+++ Capstone490/Capstone490/Models/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone490.Models
+{
+    public class Question
+    {
+        public int QuestionID { get; set; }
+
+        public string Title { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
